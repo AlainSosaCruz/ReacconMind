e43b2892056323851f89b4bf6b5812314695c7c0
Gabriel Amauri <gabrielcadena0.15@gmail.com>
2024-10-30 22:41:20 -0600

    Added class level documentation for Bot, Follower, Message and Notification entity

--- src/main/java/com/reacconmind/reacconmind/controller/BotController.java
+++ src/main/java/com/reacconmind/reacconmind/controller/BotController.java
@@ -33,7 +33,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
         RequestMethod.PUT })
         
-@Tag(name = "Bot")
+@Tag(name = "Bot", description = "Provides methods to manage Bots.")
 public class BotController {
 
 
